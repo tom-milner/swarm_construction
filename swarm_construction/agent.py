ed5bd4407711ea486dece6453f0f8ef24481fb24
Tom <tom.f.milner@gmail.com>
2025-03-27 23:40:48 +0100

    Implemented edge following.

--- swarm_construction/agent.py
+++ swarm_construction/agent.py
@@ -25,7 +25,35 @@ class Agent:
         self.direction = direction
         self.speed = speed  # Speed is measured in pixels per second!
         self.on_collision = self._do_nothing
-        self.orbit_agent = None
+
+        self._orbit_agent = None
+
+    def _move_orbit(self, scaled_speed):
+        # I don't why I did this, but this bit is implemented backwards!
+        # I didn't want to store an "orbit_angle" variable in the class,
+        # so we spin the agent directly, then position it so that it's
+        # direction is perpendicular to the orbit_agent.
+        # It works, but there's most definetely a better way!
+
+        # Apply the agent velocity as an angular velocity.
+        dist = self.radius + self._orbit_agent.radius
+        ang_vel = scaled_speed / dist
+        self.direction = (self.direction - ang_vel) % (2 * math.pi)
+
+        # Calculate the angle perpendicular to our current direction.
+        perp = self.direction - (math.pi / 2)
+
+        # Position ourselves in the orbit at that angle.
+        orbit_vec = dist * np.array([math.sin(perp), math.cos(perp)])
+        self.pos = np.subtract(self._orbit_agent.pos, orbit_vec)
+
+    def _move_vector(self, scaled_speed):
+        # Turn speed and direction into a velocity vector.
+        v = scaled_speed * np.array(
+            [math.sin(self.direction), math.cos(self.direction)]
+        )
+        # Apply vector to current position.
+        self.pos = np.add(v, self.pos)
 
     def update(self, fps):
         if fps == 0:
@@ -35,21 +63,10 @@ class Agent:
         # the frame rate.
         scaled_speed = self.speed / fps
 
-        if self.orbit_agent:
-            dist = self.radius + self.orbit_agent.radius
-            ang_vel = scaled_speed / dist
-            self.direction = (self.direction - ang_vel) % (2 * math.pi)
-            perp = self.direction - (math.pi / 2)
-            orbit_vec = dist * np.array([math.sin(perp), math.cos(perp)])
-            self.pos = np.subtract(self.orbit_agent.pos, orbit_vec)
-
+        if self._orbit_agent:
+            self._move_orbit(scaled_speed)
         else:
-            # Turn speed and direction into a velocity vector.
-            v = scaled_speed * np.array(
-                [math.sin(self.direction), math.cos(self.direction)]
-            )
-            # Apply vector to current position.
-            self.pos = np.add(v, self.pos)
+            self._move_vector(scaled_speed)
 
     def draw(self):
 
@@ -75,7 +92,26 @@ class Agent:
         if distance <= threshold:
             # Collision!
             # Work out how deep the collision is.
-            norm = diff / distance
+            if distance == 0:
+                norm = 0
+            else:
+                norm = diff / distance
             penetration = np.subtract(norm * threshold, diff)
             return (True, penetration)
-        return (False, [0, 0])
+        return (False, np.array([0, 0]))
+
+    def set_orbit_agent(self, orbit_agent):
+        self._orbit_agent = orbit_agent
+
+        # If we're already touching the edge of the agent, change our
+        # direction and start orbiting from there.
+        col = self.check_collision(orbit_agent)
+        if col[0]:
+            # Change our direction to be perpendicular to the agent.
+            diff = np.subtract(self.pos, orbit_agent.pos)
+            perp = math.atan2(-diff[1], diff[0])
+            self.direction = perp
+
+        # If we're not touching the agent, position ourselves so that we're
+        # perpendicular to it at our current direction.
+        self._move_orbit(0)
